1cb3cfee5b8be1c2ade363087985aab850331635
Borys Boiko <burbokop@gmail.com>
2022-02-28 15:01:59 +0000

    +

--- configure.sh
+++ configure.sh
@@ -11,16 +11,23 @@ link_system() {
 	fi
 }
 
-
 if [[ "$(basename -- "$0")" == "configure.sh" ]]; then
 	echo "configure.sh must be sourced"
 	echo "run by: source ./configure.sh"
 else
     echo "Setting env..."
-    export QMAKE=$PB_SDK_DIR/local/qt5/bin/qmake
-	export QT_INCLUDE_PATH=$PB_SDK_DIR/local/qt5/include
-	export QT_LIBRARY_PATH=$PB_SDK_DIR/local/qt5/lib
-	export QT_LIBRARY_PATH=$PB_SDK_DIR/local/qt5/lib
+	if [[ "$2" == "--arm" ]]; then
+ 	    echo "architecture=arm"
+		 . ../../env_set.sh >/dev/null
+		export QT_PATH=$TOOLCHAIN_PATH/$TOOLCHAIN_PREFIX/sysroot/usr/qt5
+	else
+ 	    echo "architecture=x86"
+		export QT_PATH=$PB_SDK_DIR/local/qt5
+	fi
+
+	export QMAKE=$QT_PATH/bin/qmake
+	export QT_INCLUDE_PATH=$QT_PATH/include
+	export QT_LIBRARY_PATH=$QT_PATH/lib
 
 	export LD_LIBRARY_PATH=$QT_LIBRARY_PATH:$LD_LIBRARY_PATH
 	export QT_QPA_PLATFORM=pocketbook2
@@ -34,14 +41,17 @@ fi
 
 
 ROOT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-BUILD_DIR=$ROOT_DIR/target/debug/
+BUILD_DIR=$ROOT_DIR/target
 
 echo "ROOT_DIR=$ROOT_DIR"
 echo "BUILD_DIR=$BUILD_DIR"
 
 link_system
-mkdir -p $BUILD_DIR
-cd $BUILD_DIR
+mkdir -p $BUILD_DIR/debug
+cd $BUILD_DIR/debug
+link_system
+mkdir -p $BUILD_DIR/release
+cd $BUILD_DIR/release
 link_system
 
 cd $ROOT_DIR
