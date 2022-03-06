a3e25217ff1f96a91de59880e57c8bf01b1b6b67
Borys Boiko <burbokop@gmail.com>
2022-03-06 11:41:34 +0000

    +

--- nightbuild.sh
+++ nightbuild.sh
@@ -2,4 +2,4 @@
 
 cargo install --git https://github.com/burbokop/conf_plugin.git
 cargo generate config --target armv7-unknown-linux-gnueabi
-cargo build --target armv7-unknown-linux-gnueabi
+cargo build --release
